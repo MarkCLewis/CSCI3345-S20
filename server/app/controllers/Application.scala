7be0fc2efe554a5dcb5b1b837465a5c79d1fc2f4
Mark Lewis <mlewis@trinity.edu>
2020-02-18 12:39:18 -0600

    Demonstrated forms.

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -4,20 +4,36 @@ import javax.inject._
 
 import edu.trinity.videoquizreact.shared.SharedMessages
 import play.api.mvc._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
+  def index = Action { implicit request =>
     Ok(views.html.index("<b>Testing</b>"))//SharedMessages.itWorks))
   }
 
-  def tempPage = Action {
+  def tempPage = Action { implicit request =>
     Ok(views.html.tempPage(models.TempModel.data.map(_.year).distinct))
   }
   
-  def temps(month: Int, year: Int) = Action {
+  def temps(month: Int, year: Int) = Action { implicit request =>
     Ok(views.html.tempMonth(month, year, models.TempModel.data.
       filter(td => td.year == year && td.month == month)))
+  }
+
+  def tempsPost = Action.async { implicit request => Future {
+    val oparams = request.body.asFormUrlEncoded
+    oparams.map { params =>
+      try {
+        val month = params("month")(0).toInt
+        val year = params("year")(0).toInt
+        Ok(views.html.tempMonth(month, year, models.TempModel.data.
+          filter(td => td.year == year && td.month == month)))
+      } catch {
+        case ex: NumberFormatException => Ok("NaN")
+      }
+    }.getOrElse(Ok("You screwed up.")) }
   }
 }
