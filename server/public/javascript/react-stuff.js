6f7af5af40c9e679db578fe9c0622a53437fc4d8
Mark Lewis <mlewis@trinity.edu>
2020-03-05 13:43:02 -0600

    Added a bit of react code for a form element.

--- server/public/javascript/react-stuff.js
+++ server/public/javascript/react-stuff.js
@@ -19,10 +19,26 @@ function HelloWorld2(props) {
   return React.createElement('h1', null, `Hello ${props.name}!`);
 }
 
+class InputComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ""};
+  }
+
+  render() {
+    return React.createElement('input', {type: 'text', value: this.state.text, onChange: e => this.changeHandler(e)});
+  }
+
+  changeHandler(e) {
+    this.setState({text: e.target.value});
+  }
+}
+
 ReactDOM.render(
   React.createElement('div', null,
     React.createElement(HelloWorld, {name: 'Pat'}, null),
-    React.createElement(HelloWorld2, {name: 'Morgan'}, null)
+    React.createElement(HelloWorld2, {name: 'Morgan'}, null),
+    React.createElement(InputComponent)
   ),
   document.getElementById('react-root')
 )
